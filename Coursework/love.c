596db36d865f4c170ca34fe0ceb6fcca03be38f4
Lewis Smith <lewissmiths@aol.com>
2015-11-11 10:54:06 +0000

    Update love.c

--- Coursework/love.c
+++ Coursework/love.c
@@ -1,4 +1,4 @@
-
+#include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
 
@@ -11,8 +11,67 @@
 #include "library/a_d/advance_ad_scan/e_prox.h"
 #include "library/codec/e_sound.h"
 #include "library/motor_led/e_epuck_ports.h"
+#include "library/camera/fast_2_timer/e_poxxxx.h"
 
 #include "love.h"
+/*
+long isGreenVisable;
+char gbuffer[160];
+int gnumbuffer[80];
+
+
+//custom cam picture load - Simon and Shaun
+void takeImage(){
+	e_poxxxx_launch_capture((char *)gbuffer);
+    while(!e_poxxxx_is_img_ready()){};
+}
+
+// Image processing removes useless information - Simon and Shaun
+void processImage(){	
+	long i;
+	int green, red, vis;
+	for(i=0; i<80; i++){
+		//RGB turned into an integer value for comparison
+		red = (gbuffer[2*i] & 0xF8);
+		green = (((gbuffer[2*i] & 0x07) << 5) | ((gbuffer[2*i+1] & 0xE0) >> 3));
+		//blue = ((gfbwbuffer[2*i+1] & 0x1F) << 3); we don't need blue for looking for green.
+		if(green > red + 50){ //Green is usually much higher then red due the the extra bit place in RGB565
+			gnumbuffer[i] = 1;
+			vis +=1;
+		}else{
+			gnumbuffer[i] = 0;
+		}
+		//If Green is visable then isRedVisable turns to true
+		if(vis>0){
+			isGreenVisable = 1;
+		}else{
+			isGreenVisable = 0;
+		}
+	}	
+}
+
+//Decide which way to turn based on the number of true pixels. - Simon and Shaun
+int gturnDirection(){
+	int sumL = 0;
+	int sumR = 0;
+	int i;
+	for(i=0;i<40;i++){
+		sumL += gnumbuffer[i];
+		sumR += gnumbuffer[i+40];
+	}
+	if(sumL<sumR){ 
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+void setUpCamera(void){
+	e_poxxxx_init_cam();
+	e_poxxxx_config_cam(0,(ARRAY_HEIGHT - 4)/2,640,4,8,4,RGB_565_MODE);
+	e_poxxxx_set_mirror(1,1);
+	e_poxxxx_write_cam_registers(); 
+}*/
 
 void love(void)
 {
@@ -21,6 +80,7 @@ void love(void)
 	e_init_ad_scan(ALL_ADC);
 	
 	e_calibrate_ir();
+	setUpCamera();
 
 	e_set_speed_left(500);
 	e_set_speed_right(500);
@@ -48,6 +108,8 @@ void love(void)
 
 		for(i=0; i<100000; i++) { asm("nop"); }
 	}
+
+	takeImage();
 
 	//(3) Given the object is in close proximity flash all LED's
 	while(1)
